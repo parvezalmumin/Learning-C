093ce0416bd0f5b9475cf9f14a4839794d977a8b
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2025-11-03 18:48:28 -0800

    Excape Sequence Practice Code Added

---
+++ Class 01/escapesequence.c
@@ -0,0 +1,15 @@
+ #include <stdio.h>
+ int main()
+ {
+    printf("Hello World");
+    printf("This line is witout excape sequence");
+    printf("\n");
+    printf("This line is writen after using excape sequence");
+   printf("\n");
+   printf("\n");
+printf("After using two escape sequence");
+printf("Learning Escape Sequence\n");
+printf("used excape sequence in one line");
+
+    return 0;
+ }
