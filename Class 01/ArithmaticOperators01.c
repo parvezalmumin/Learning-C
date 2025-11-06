e3d94c4f9872533d372b6be6946cbb698c8b90df
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2025-11-05 22:10:59 -0800

    Variables and Arithmetic code examples added

---
+++ Class 01/ArithmaticOperators01.c
@@ -0,0 +1,17 @@
+ #include<stdio.h>
+ int main() {
+    int x;
+    x = 5;
+    int y = 2;
+
+   printf("%d",x*y);
+   printf("\n");
+  printf("%d\n",x+y);
+ printf("%d\n",x-y);
+  printf("%d\n",x/y);
+
+  int z = x + y;
+   printf("%d",z);
+
+  return 0;
+ }
