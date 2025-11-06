c365a3e386620d30344d2086a3c30213e37880c3
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2025-11-06 07:22:57 -0800

    Percantge of Five Subject practice code added

---
+++ Class 01/PercentageOfFiveSubjects.c
@@ -0,0 +1,16 @@
+ #include<stdio.h>
+ int  main () {
+
+    float m1 = 90;
+    float m2 = 91;
+    float m3 = 92;
+    float m4 = 93;
+    float m5 = 94;
+     float p = (m1 + m2 + m3 + m4 + m5)/5;
+
+printf("Percentage is: %f", p);
+
+
+return 0;
+
+ }
