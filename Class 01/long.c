55df2e09a265e084c5866453927d187ecc1f9ad6
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2025-11-08 12:14:51 -0800

    Long practice code added

---
+++ Class 01/long.c
@@ -0,0 +1,11 @@
+ #include <stdio.h>
+
+int main() {
+    long distance = 9876543210;     // large integer value
+    long population = 170000000;    // Bangladesh population (example)
+
+    printf("Distance: %ld\n", distance);
+    printf("Population: %ld\n", population);
+
+    return 0;
+}
