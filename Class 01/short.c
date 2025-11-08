2dfef03b9d3e74a6a44fe6634db1700ce6276a64
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2025-11-08 12:04:42 -0800

    Short code practice added

---
+++ Class 01/short.c
@@ -0,0 +1,11 @@
+ #include <stdio.h>
+
+int main() {
+    short age = 25;       // declaring a short integer
+    short year = 2025;
+
+    printf("Age: %d\n", age);
+    printf("Year: %d\n", year);
+
+    return 0;
+}
