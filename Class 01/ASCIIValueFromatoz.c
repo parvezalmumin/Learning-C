0306d4d33c041e4b686e6325c0f0b9f5ba2179cb
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2025-11-07 23:50:56 -0800

    ASCII Value of Lowercase code practice added

---
+++ Class 01/ASCIIValueFromatoz.c
@@ -0,0 +1,59 @@
+ #include <stdio.h>
+
+int main() {
+    char a = 'a'; 
+    char b = 'b'; 
+    char c = 'c'; 
+    char d = 'd';
+    char e = 'e';
+    char f = 'f';
+    char g = 'g';
+    char h = 'h';
+    char i = 'i';
+    char j = 'j';
+    char k = 'k';
+    char l = 'l';
+    char m = 'm';
+    char n = 'n';
+    char o = 'o';
+    char p = 'p';
+    char q = 'q';
+    char r = 'r';
+    char s = 's';
+    char t = 't';
+    char u = 'u';
+    char v = 'v';
+    char w = 'w';
+    char x = 'x';
+    char y = 'y';
+    char z = 'z';
+
+    printf("The ASCII value of %c is %d\n", a, a);
+    printf("The ASCII value of %c is %d\n", b, b);
+    printf("The ASCII value of %c is %d\n", c, c);
+    printf("The ASCII value of %c is %d\n", d, d);
+    printf("The ASCII value of %c is %d\n", e, e);
+    printf("The ASCII value of %c is %d\n", f, f);
+    printf("The ASCII value of %c is %d\n", g, g);
+    printf("The ASCII value of %c is %d\n", h, h);
+    printf("The ASCII value of %c is %d\n", i, i);
+    printf("The ASCII value of %c is %d\n", j, j);
+    printf("The ASCII value of %c is %d\n", k, k);
+    printf("The ASCII value of %c is %d\n", l, l);
+    printf("The ASCII value of %c is %d\n", m, m);
+    printf("The ASCII value of %c is %d\n", n, n);
+    printf("The ASCII value of %c is %d\n", o, o);
+    printf("The ASCII value of %c is %d\n", p, p);
+    printf("The ASCII value of %c is %d\n", q, q);
+    printf("The ASCII value of %c is %d\n", r, r);
+    printf("The ASCII value of %c is %d\n", s, s);
+    printf("The ASCII value of %c is %d\n", t, t);
+    printf("The ASCII value of %c is %d\n", u, u);
+    printf("The ASCII value of %c is %d\n", v, v);
+    printf("The ASCII value of %c is %d\n", w, w);
+    printf("The ASCII value of %c is %d\n", x, x);
+    printf("The ASCII value of %c is %d\n", y, y);
+    printf("The ASCII value of %c is %d\n", z, z);
+
+    return 0;
+}
