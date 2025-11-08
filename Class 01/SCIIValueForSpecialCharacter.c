21de0ba8424738998c8197ab2fb5c01cffad13d1
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2025-11-07 23:52:52 -0800

    ASCII Values for speical Characters Added

---
+++ Class 01/SCIIValueForSpecialCharacter.c
@@ -0,0 +1,67 @@
+ #include <stdio.h>
+
+int main() {
+    char a = '!'; 
+    char b = '@'; 
+    char c = '#'; 
+    char d = '$'; 
+    char e = '%'; 
+    char f = '^'; 
+    char g = '&'; 
+    char h = '*'; 
+    char i = '('; 
+    char j = ')'; 
+    char k = '-'; 
+    char l = '_'; 
+    char m = '='; 
+    char n = '+'; 
+    char o = '['; 
+    char p = ']'; 
+    char q = '{'; 
+    char r = '}'; 
+    char s = ';'; 
+    char t = ':'; 
+    char u = '\''; 
+    char v = '"'; 
+    char w = ','; 
+    char x = '.'; 
+    char y = '<'; 
+    char z = '>'; 
+    char aa = '/'; 
+    char bb = '?'; 
+    char cc = '\\'; 
+    char dd = '|';
+
+    printf("The ASCII value of %c is %d\n", a, a);
+    printf("The ASCII value of %c is %d\n", b, b);
+    printf("The ASCII value of %c is %d\n", c, c);
+    printf("The ASCII value of %c is %d\n", d, d);
+    printf("The ASCII value of %c is %d\n", e, e);
+    printf("The ASCII value of %c is %d\n", f, f);
+    printf("The ASCII value of %c is %d\n", g, g);
+    printf("The ASCII value of %c is %d\n", h, h);
+    printf("The ASCII value of %c is %d\n", i, i);
+    printf("The ASCII value of %c is %d\n", j, j);
+    printf("The ASCII value of %c is %d\n", k, k);
+    printf("The ASCII value of %c is %d\n", l, l);
+    printf("The ASCII value of %c is %d\n", m, m);
+    printf("The ASCII value of %c is %d\n", n, n);
+    printf("The ASCII value of %c is %d\n", o, o);
+    printf("The ASCII value of %c is %d\n", p, p);
+    printf("The ASCII value of %c is %d\n", q, q);
+    printf("The ASCII value of %c is %d\n", r, r);
+    printf("The ASCII value of %c is %d\n", s, s);
+    printf("The ASCII value of %c is %d\n", t, t);
+    printf("The ASCII value of %c is %d\n", u, u);
+    printf("The ASCII value of %c is %d\n", v, v);
+    printf("The ASCII value of %c is %d\n", w, w);
+    printf("The ASCII value of %c is %d\n", x, x);
+    printf("The ASCII value of %c is %d\n", y, y);
+    printf("The ASCII value of %c is %d\n", z, z);
+    printf("The ASCII value of %c is %d\n", aa, aa);
+    printf("The ASCII value of %c is %d\n", bb, bb);
+    printf("The ASCII value of %c is %d\n", cc, cc);
+    printf("The ASCII value of %c is %d\n", dd, dd);
+
+    return 0;
+}
