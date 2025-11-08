c0da1ad217b865a45000f4a6b351a97960e0cede
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2025-11-08 12:15:52 -0800

    Long Long Practice code added

---
+++ Class 01/LongLong.c
@@ -0,0 +1,11 @@
+ #include <stdio.h>
+
+int main() {
+    long long worldPopulation = 8000000000;   // 8 billion
+    long long distanceToMoon = 384400000;     // in meters
+
+    printf("World Population: %lld\n", worldPopulation);
+    printf("Distance to Moon: %lld meters\n", distanceToMoon);
+
+    return 0;
+}
