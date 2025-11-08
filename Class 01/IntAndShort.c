ebee9ab859ab41d352bf49d4acb3e8166694d1d4
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2025-11-08 12:06:02 -0800

    Short and Integer practice code added

---
+++ Class 01/IntAndShort.c
@@ -0,0 +1,11 @@
+ #include <stdio.h>
+
+int main() {
+    short age = 25;
+    int population = 170000000;
+
+    printf("Age: %d\n", age);
+    printf("Population: %d\n", population);
+
+    return 0;
+}
