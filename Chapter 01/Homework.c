6ee3020ed06e1230ce2d89a923dfade1cf48a8a2
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2026-01-04 23:34:07 -0800

    Homework Added from College Wallah Tutorial

---
+++ Chapter 01/Homework.c
@@ -0,0 +1,10 @@
+//Homework From College Wallah
+//Area Of Circle
+#include<stdio.h>
+int main(){
+   float pi = 22.0/7;
+   float r = 5;
+   float A = pi * r * r;
+   printf("Area of Circle is: %f", A );
+    return 0;
+}
