d01ccacdfec859c83dfdd1d7e38d8751625d0d6f
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2026-01-04 22:54:25 -0800

    Problem 02 Added

---
+++ Chapter 01/Problem02.c
@@ -0,0 +1,9 @@
+//Volume Of Sphere
+#include <stdio.h>
+int main(){
+     int r = 4;
+     float pi = 22.0/7;
+     float V = (4 * pi * r * r * r ) / 3;
+     printf("The Volume of Sphere is: %f",V);
+    return 0;
+}
