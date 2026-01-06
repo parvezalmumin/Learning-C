471c70b5f318a5aeecf0841827b4086f0003af2a
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2026-01-05 22:13:10 -0800

    Problem 05(Simple Interest) added

---
+++ Chapter 01/Problem05.c
@@ -0,0 +1,12 @@
+//Simple Iterest
+#include<stdio.h>
+int main(){
+    float p,r,t,si;
+    p = 100;
+    r = 10;
+    t = 2;
+    si = (p*r*t)/100;
+    printf("Simple Interest = %f", si);
+    return 0;
+
+}
