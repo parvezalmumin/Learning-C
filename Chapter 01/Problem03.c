ecf1a1e7f856c156e0e5ce80310963e254e8ef02
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2026-01-05 21:07:31 -0800

    Problem 03: Percentage of five numbers added.

---
+++ Chapter 01/Problem03.c
@@ -0,0 +1,12 @@
+//Percentage of five numbers
+#include<stdio.h>
+int main(){
+    float marks01 = 89;
+    float marks02 = 91;
+    float marks03 = 85;
+    float marks04 = 79;
+    float marks05 = 96;
+    float percentage = (marks01 + marks02 + marks03 + marks04 + marks05)/5;
+    printf("Percentage of Fice Numbers: %f",percentage);
+    return 0;
+}
