2f63aeabd1d5a3014997482342ec36445685ee64
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2026-01-26 22:48:40 -0800

    Problem 05 Added

--- Chapter 02/Problem05.c
+++ Chapter 02/Problem05.c
@@ -5,20 +5,21 @@
 int main() {
     int l;
     printf("Enter Length: ");
-    scanf("%d",&l);
+    scanf("%d", &l);
     
     int b;
     printf("Enter Breadth: ");
-    scanf("%d",&b);
+    scanf("%d", &b);
 
     int a = l * b;
     int p = 2 * (l + b);
 
-    if(a>b){
-        printf("Area is greater than Perimeter");
+    if(a > p){
+        printf("Area is greater than Perimeter\n");
     }
     else{
-       printf("Area is not greater than Perimeter");
+        printf("Area is not greater than Perimeter\n");
     }
+
     return 0;
 }
