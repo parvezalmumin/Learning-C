b15aef8b4effefceb1943c4b2daddb1bdd48f7ee
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2026-01-31 11:51:19 -0800

    Problem 08 added

---
+++ Chapter 02/Problem08.c
@@ -0,0 +1,15 @@
+//Take positive integer input and tell if it is divisible by 5 or 3.
+//Take positive integer input and tell if it divisible by 5 and 3
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if(n % 5 == 0 && n % 3 == 0){
+        printf("The number is divisible by 5 and 3");
+    }
+    else{
+        printf("The number is not divisible by 5 and 3");
+    }
+}
